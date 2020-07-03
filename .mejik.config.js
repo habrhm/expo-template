d5492bb355d50e112b81437805d57bda16669473
habrhm <m.haris.abdurrahman@gmail.com>
2020-07-03 12:33:11 +0700

    add config

---
+++ .mejik.config.js
@@ -0,0 +1,7 @@
+export default { 
+    //---start-configs--- 
+    GRAPHQL_URL: 'https://mejikarttodo1593672742653.microgen.mejik.id/graphql', 
+    VAR_A: 'aaa', 
+    VAR_B: 'bbb', 
+    //---end-configs--- 
+    }
